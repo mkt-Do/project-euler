49713dc2bb285971a6691642075db4acc65d82b4
mkt-Do <mkt.koro.mikuru@gmail.com>
2018-10-01 11:30:16 +0900

    add assert

--- src/test/scala/jp/mkt/euler/projects/P004Spec.scala
+++ src/test/scala/jp/mkt/euler/projects/P004Spec.scala
@@ -5,10 +5,10 @@ import org.scalatest._
 class P004Spec extends FunSuite {
 
   test("9119 is palindrome") {
-    P004.palindrome(9119)
+    assert(P004.palindrome(9119))
   }
 
   test("1234 is not palindrome") {
-    !P004.palindrome(1234)
+    assert(!P004.palindrome(1234))
   }
 }
